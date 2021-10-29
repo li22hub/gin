d7aca48dc07e0fcd4fb0312e6a800a6ac4b57bb7
lizheng <1124889236@qq.com>
2021-10-29 16:30:27 +0800

    first commit

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("nihao")
+	//fmt.Println("hello world!")
 
+	r := gin.Default()
+	r.POST("/admin", func(c *gin.Context) {
+		c.String(http.StatusOK,"hello world")
+	})
+	r.Run(":8888")
 }
